13429b4f2a7ded02270414fc15ce8692c333d199
Sören Biester <soeren.biester@gmail.com>
2025-04-04 16:47:14 +0200

    Static

--- adventure/src/main/java/org/myrdn/adventure/Main.java
+++ adventure/src/main/java/org/myrdn/adventure/Main.java
@@ -1,19 +1,10 @@
 package org.myrdn.adventure;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Höhe: ");
-            int ySize = scanner.nextInt();
-            System.out.println();
-            System.out.print("Breite: ");
-            int xSize = scanner.nextInt();
-            System.out.println();
-
-            Game game = new Game(xSize, ySize, "Myrdn");
-            game.init();
-        }
+        int ySize = 10;
+        int xSize = 20;
+        Game game = new Game(xSize, ySize, "Myrdn");
+        game.init();
     }
 }
