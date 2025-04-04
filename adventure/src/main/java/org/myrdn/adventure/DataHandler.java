99d1c95992b36c0a2c4fb747c0e7ea2101db3fea
Sören Biester <soeren.biester@gmail.com>
2025-04-04 05:01:45 +0200

    DataHander hinzugefügt

---
+++ adventure/src/main/java/org/myrdn/adventure/DataHandler.java
@@ -0,0 +1,26 @@
+package org.myrdn.adventure;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class DataHandler {
+public static void savePlayer(Player player) throws IOException {
+    String fileName= player.getName() + ".bin";
+    FileOutputStream fos = new FileOutputStream(fileName);
+    ObjectOutputStream oos = new ObjectOutputStream(fos);
+    oos.writeObject(player);
+    oos.close();
+}
+
+public static Player loadPlayer(String playerName) throws IOException, ClassNotFoundException {
+   String fileName= playerName + ".bin";
+   FileInputStream fin = new FileInputStream(fileName);
+   ObjectInputStream ois = new ObjectInputStream(fin);
+   Player player = (Player) ois.readObject();
+   ois.close();
+   return player;
+}
+}
