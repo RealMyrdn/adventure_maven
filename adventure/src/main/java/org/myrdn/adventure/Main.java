da42468f7db2d573da94c86806519d7c8a4b6664
Sören Biester <soeren.biester@gmail.com>
2025-04-04 06:00:07 +0200

    Variable Map Größe hinzugefügt

--- adventure/src/main/java/org/myrdn/adventure/Main.java
+++ adventure/src/main/java/org/myrdn/adventure/Main.java
@@ -1,11 +1,15 @@
 package org.myrdn.adventure;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int ySize = 10;
-        int xSize = 20;
+        try (Scanner scanner = new Scanner(System.in)) {
+            int ySize = scanner.nextInt();
+            int xSize = scanner.nextInt();
 
-        Game game = new Game(xSize, ySize, "Myrdn");
-        game.init();
+            Game game = new Game(xSize, ySize, "Myrdn");
+            game.init();
+        }
     }
 }
