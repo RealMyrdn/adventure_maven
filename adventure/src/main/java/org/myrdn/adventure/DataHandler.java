6da8308393591eca28a0ca6497a5e12e96d27402
Sören Biester <soeren.biester@gmail.com>
2025-04-04 05:05:08 +0200

    message: Formatierung geändert

--- adventure/src/main/java/org/myrdn/adventure/DataHandler.java
+++ adventure/src/main/java/org/myrdn/adventure/DataHandler.java
@@ -7,20 +7,20 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class DataHandler {
-public static void savePlayer(Player player) throws IOException {
-    String fileName= player.getName() + ".bin";
-    FileOutputStream fos = new FileOutputStream(fileName);
-    ObjectOutputStream oos = new ObjectOutputStream(fos);
-    oos.writeObject(player);
-    oos.close();
-}
+    public static void savePlayer(Player player) throws IOException {
+        String fileName= player.getName() + ".bin";
+        FileOutputStream fos = new FileOutputStream(fileName);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(player);
+        oos.close();
+    }
 
-public static Player loadPlayer(String playerName) throws IOException, ClassNotFoundException {
-   String fileName= playerName + ".bin";
-   FileInputStream fin = new FileInputStream(fileName);
-   ObjectInputStream ois = new ObjectInputStream(fin);
-   Player player = (Player) ois.readObject();
-   ois.close();
-   return player;
-}
+    public static Player loadPlayer(String playerName) throws IOException, ClassNotFoundException {
+    String fileName= playerName + ".bin";
+    FileInputStream fin = new FileInputStream(fileName);
+    ObjectInputStream ois = new ObjectInputStream(fin);
+    Player player = (Player) ois.readObject();
+    ois.close();
+    return player;
+    }
 }
