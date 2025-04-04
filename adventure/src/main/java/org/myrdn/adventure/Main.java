8e154eba0a5e2bd4395c9260e14964fb109db60b
Sören Biester <soeren.biester@gmail.com>
2025-04-04 06:02:14 +0200

    Eingabe angepasst

--- adventure/src/main/java/org/myrdn/adventure/Main.java
+++ adventure/src/main/java/org/myrdn/adventure/Main.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Höhe: ");
             int ySize = scanner.nextInt();
+            System.out.println();
+            System.out.print("Breite: ");
             int xSize = scanner.nextInt();
+            System.out.println();
 
             Game game = new Game(xSize, ySize, "Myrdn");
             game.init();
