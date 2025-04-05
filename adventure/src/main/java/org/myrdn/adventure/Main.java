053a1379048522a06020cd9e1a6960dcc631e269
Sören Biester <soeren.biester@gmail.com>
2025-04-05 07:44:58 +0200

    line added

--- adventure/src/main/java/org/myrdn/adventure/Main.java
+++ adventure/src/main/java/org/myrdn/adventure/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int ySize = 10;
         int xSize = 20;
+        
         Game game = new Game(xSize, ySize, "Myrdn");
         game.init();
     }
