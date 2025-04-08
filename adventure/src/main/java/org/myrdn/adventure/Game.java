151e0738451447b138f71a2d018b7aff3b9894d9
realMyrdn <soeren.biester@gmail.com>
2025-04-08 16:16:35 +0200

    sync

--- adventure/src/main/java/org/myrdn/adventure/Game.java
+++ adventure/src/main/java/org/myrdn/adventure/Game.java
@@ -20,11 +20,16 @@ public class Game {
     private final SaveGame savegame;
     private DataHandler datahandler = new DataHandler();
     private Renderer renderer;
+    private int xPos;
+    private int yPos;
+    private ArrayList<GameObject> objects = new ArrayList<>();
 
     public Game(int xSize, int ySize, String name) throws IOException  {
         this.generator    = new Generator(xSize, ySize, datahandler.loadObjects(), datahandler.loadItems());
         this.house        = new House(this.generator.getStartPosition(), this.generator.getRooms());
         this.player       = new Player(name ,house.getStartPosition());
+        this.xPos         = this.player.getPosition()[1];
+        this.yPos         = this.player.getPosition()[0];
         this.savegame     = new SaveGame(this.player, this.house);
         try {
             this.renderer = new Renderer(xSize, ySize);
@@ -47,88 +52,54 @@ public class Game {
     public void loop() {
         boolean isRunning = true;
         boolean firstDraw = true;
+
         try {
-        KeyStroke keyStroke;
-        KeyType keyType;
-        ArrayList<KeyStroke> keyStrokes = new ArrayList<>();
+            KeyStroke keyStroke;
+            KeyType keyType;
+            ArrayList<KeyStroke> keyStrokes = new ArrayList<>();
 
-        while (isRunning && this.renderer.getScreen() != null) {
-            if(firstDraw) {
+            while (isRunning && this.renderer.getScreen() != null) {
+                this.xPos         = this.player.getPosition()[1];
+                this.yPos         = this.player.getPosition()[0];
+                if(firstDraw) {
+                    this.renderer.printMap(this.house.drawMap(), player.getPosition()[1], player.getPosition()[0]);
+                    this.renderer.printRoomDescription(this.house.getRoom(player.getPosition()[1], player.getPosition()[0]).getRoomInfo(), 20);
+                    this.renderer.printInputLine(keyStrokes);
+                    this.renderer.renderFrame();
+                    firstDraw = false;
+                }
+
+                while (true) {
+                    keyStroke = this.renderer.getTerminal().readInput();
+                    keyType = keyStroke.getKeyType();
+
+                    if (keyType == KeyType.Enter) {
+                        break;
+                    }
+
+                    if (keyStrokes.size() < 40 && keyType == KeyType.Character) {
+                        keyStrokes.add(keyStroke);
+                    } else if (!keyStrokes.isEmpty() && keyType == KeyType.Backspace) {
+                        keyStrokes.removeLast();
+                    }
+
+                    this.renderer.printInputLine(keyStrokes);
+                    this.renderer.renderFrame();
+                }
+
+                executeInstructions(processKeyStrokes(keyStrokes));
+                keyStrokes.clear();
+
                 this.renderer.printMap(this.house.drawMap(), player.getPosition()[1], player.getPosition()[0]);
-                this.renderer.printRoomDescription(
-                    this.house.getRoom(player.getPosition()[1], player.getPosition()[0]).getRoomInfo(), 
-                    20
-                );
-                this.renderer.printInputLine(keyStrokes);
-                this.renderer.renderFrame();
-                firstDraw = false;
-            }
-            while (true) {
-                keyStroke = this.renderer.getTerminal().readInput();
-                keyType = keyStroke.getKeyType();
-
-                // System.out.println("KeyType: " + keyType);
-                // System.out.println("Char: " + keyStroke.getCharacter());
-
-                if (keyType == KeyType.Enter) {
-                    break;
-                }
-
-                if (keyStrokes.size() < 40 && keyType == KeyType.Character) {
-                    keyStrokes.add(keyStroke);
-                } else if (!keyStrokes.isEmpty() && keyType == KeyType.Backspace) {
-                    keyStrokes.removeLast();
-                }
-
+                this.renderer.printRoomDescription(this.house.getRoom(player.getPosition()[1], player.getPosition()[0]).getRoomInfo(), 20);
                 this.renderer.printInputLine(keyStrokes);
                 this.renderer.renderFrame();
             }
 
-            executeInstructions(processKeyStrokes(keyStrokes));
-            keyStrokes.clear();
-
-            this.renderer.printMap(this.house.drawMap(), player.getPosition()[1], player.getPosition()[0]);
-            this.renderer.printRoomDescription(this.house.getRoom(player.getPosition()[1], player.getPosition()[0]).getRoomInfo(), 20);
-            this.renderer.printInputLine(keyStrokes);
-            this.renderer.renderFrame();
+        } catch (IOException e) {
+            System.out.println("Fehler im Game-Loop:");
+            System.out.println(e);
         }
-
-    } catch (IOException e) {
-        System.out.println("Fehler im Game-Loop:");
-        System.out.println(e);
-    }
-        // boolean isRunning = true;
-        // try {
-        //     KeyStroke keyStroke;
-        //     KeyType keyType;
-        //     ArrayList<KeyStroke> keyStrokes = new ArrayList<>();
-        //     while(isRunning) {
-        //         this.renderer.printMap(this.house.drawMap(), player.getPosition()[1], player.getPosition()[0]);
-        //         this.renderer.printRoomDescription(this.house.getRoom(player.getPosition()[1], player.getPosition()[0]).getRoomInfo(), 20);
-        //         while(true) {
-        //             keyStroke = this.renderer.getTerminal().readInput();
-        //             keyType = keyStroke.getKeyType();
-        //             System.out.println("KeyType: " + keyType);
-        //             System.out.println("Char: " + keyStroke.getCharacter());
-        //             if(keyType == KeyType.Enter) {
-        //                 break;
-        //             }
-        //             if(keyStrokes.size() < 40 && keyType == KeyType.Character) {
-        //                 keyStrokes.add(keyStroke);
-        //                 this.renderer.printInputLine(keyStrokes);
-        //             }
-        //             if(!keyStrokes.isEmpty() && keyType == KeyType.Backspace) {
-        //                 keyStrokes.removeLast();
-        //                 this.renderer.printInputLine(keyStrokes);
-        //             }
-        //         } 
-        //         executeInstructions(processKeyStrokes(keyStrokes));
-        //         keyStrokes.clear();
-        //         this.renderer.printInputLine(keyStrokes);
-        //     }
-        // } catch (IOException e) {
-        //     System.out.println(e);
-        // }
     }
 
     public ArrayList<String> processKeyStrokes(ArrayList<KeyStroke> keyStrokes) throws IOException {
@@ -169,8 +140,18 @@ public class Game {
             case "untersuche" -> {
                 switch(instructions.get(1)) {
                     case "raum" -> {
-                        this.renderer.printDescription(this.house.getRoom(this.player.getPosition()[1], this.player.getPosition()[0]).getRoomObjects(), 5);
+                        this.renderer.printDescription(this.house.getRoom(xPos, yPos).getRoomObjects(), 5);
                         this.renderer.renderFrame();
+                    }
+
+                    default -> {
+                        objects = this.house.getRoom(xPos, yPos).getObjects();
+                        for(GameObject object : objects) {
+                            if(object.getName().toLowerCase().equals(instructions.get(1))) {
+                                this.renderer.printObjectDescription(object.getDescription(), object.getHiddenItems(), 15);
+                                this.renderer.renderFrame();
+                            }
+                        }
                     }
                 }
             }
--- adventure/src/main/java/org/myrdn/adventure/Renderer.java
+++ adventure/src/main/java/org/myrdn/adventure/Renderer.java
@@ -137,21 +137,54 @@ public class Renderer {
             this.textGraphics.putString(x + 1, y + 1, cells[x]);
         }
     }
+    public void clearTextField(String backgroundColor, String foregroundColor, int height) {
+        this.textGraphics.setBackgroundColor(TextColor.ANSI.valueOf(backgroundColor));
+        this.textGraphics.setForegroundColor(TextColor.ANSI.valueOf(foregroundColor));
+        this.textGraphics.fillRectangle(new TerminalPosition(40, height), new TerminalSize(80, 8), ' ');
+    }
 
     public void printRoomDescription(String description, int height) {
-        this.textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
-        this.textGraphics.setForegroundColor(TextColor.ANSI.CYAN_BRIGHT);
-        this.textGraphics.fillRectangle(new TerminalPosition(40, height), new TerminalSize(80, 8), ' ');
+        clearTextField("BLACK", "CYAN_BRIGHT", height);
         String[] rows = description.split("\n");
         for (int y = 0; y < rows.length; y++) {
             this.textGraphics.putString(40, height + y, rows[y]);
         }
     }
 
+    public void printObjectDescription(String description, ArrayList<ItemObject> itemObjects, int height) {
+        clearTextField("BLACK", "GREEN_BRIGHT", height);
+        
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] splitDescription = description.split(" ");
+        
+        int charCounter = 0;
+
+        for(String word : splitDescription) {
+            if(charCounter + word.length() < 60) {
+                stringBuilder.append(word).append(" ");
+                charCounter += word.length() + 1;
+            } else {
+                charCounter = 0;
+                charCounter += word.length();
+                stringBuilder.append("\n").append(word).append(" ");
+            }
+        }
+
+        if(!itemObjects.isEmpty()) {
+            stringBuilder.append("Das Objekte enthält folgende Gegenstände: \n");
+            for(ItemObject item : itemObjects) {
+                stringBuilder.append(item.getName()).append(" ");
+            }
+        }
+        String objectDescription = stringBuilder.toString();
+        String[] rows = objectDescription.split("\n");
+        for (int y = 0; y < rows.length; y++) {
+            this.textGraphics.putString(40, height + y, rows[y]);
+        }
+    }
+
     public void printDescription(String description, int height) {
-        this.textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
-        this.textGraphics.setForegroundColor(TextColor.ANSI.CYAN_BRIGHT);
-        this.textGraphics.fillRectangle(new TerminalPosition(40, height), new TerminalSize(80, 8), ' ');
+        clearTextField("BLACK", "CYAN_BRIGHT", height);
         String[] rows = description.split("\n");
         for (int y = 0; y < rows.length; y++) {
             this.textGraphics.putString(40, height + y, rows[y]);
@@ -171,166 +204,3 @@ public class Renderer {
         }
     }
 }
-
-// package org.myrdn.adventure;
-
-// import java.awt.Font;
-// import java.awt.GraphicsEnvironment;
-// import java.io.IOException;
-// import java.io.InputStream;
-// import java.util.ArrayList;
-
-// import com.googlecode.lanterna.SGR;
-// import com.googlecode.lanterna.TerminalPosition;
-// import com.googlecode.lanterna.TerminalSize;
-// import com.googlecode.lanterna.TextColor;
-// import com.googlecode.lanterna.graphics.TextGraphics;
-// import com.googlecode.lanterna.input.KeyStroke;
-// import com.googlecode.lanterna.screen.Screen;
-// import com.googlecode.lanterna.screen.TerminalScreen;
-// import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-// import com.googlecode.lanterna.terminal.Terminal;
-// import com.googlecode.lanterna.terminal.swing.SwingTerminalFontConfiguration;
-// import com.googlecode.lanterna.terminal.swing.SwingTerminalFrame;
-
-// public class Renderer {
-
-//     private final Font myFont;
-//     private final SwingTerminalFontConfiguration myFontConfiguration;
-//     private Terminal terminal;
-//     private Screen screen;
-//     private TextGraphics textGraphics;
-//     private final TerminalSize size;
-//     private final int xSize;
-//     private final int ySize;
-//     private boolean mapFound;
-
-//     public Renderer(int xSize, int ySize) throws IOException {
-//         this.size = new TerminalSize(120, 40);
-//         this.myFont = loadFontFromResources("/fonts/JetBrainsMono-Regular.ttf", 14f);
-//         this.myFontConfiguration = SwingTerminalFontConfiguration.newInstance(myFont);
-//         this.xSize = xSize;
-//         this.ySize = ySize;
-//         this.mapFound = false;
-//     }
-
-//     public Terminal getTerminal() {
-//         return this.terminal;
-//     }
-
-//     public Screen getScreen() {
-//         return this.screen;
-//     }
-
-//     public void setMapFound(boolean mapFound) {
-//         this.mapFound = mapFound;
-//     }
-
-//         private Font loadFontFromResources(String path, float size) {
-//         try (InputStream fontStream = getClass().getResourceAsStream(path)) {
-//             if (fontStream == null) {
-//                 throw new IOException("Font-Datei nicht gefunden: " + path);
-//             }
-//             Font font = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(size);
-//             GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
-//             return font;
-//         } catch (Exception e) {
-//             System.err.println("Fehler beim Laden des Fonts, es wird der Standardfont verwendet.");
-//             return new Font("Monospaced", Font.PLAIN, (int) size);
-//         }
-//     }
-
-//     public void initScreen() throws IOException {
-//         DefaultTerminalFactory factory = new DefaultTerminalFactory().setTerminalEmulatorFontConfiguration(myFontConfiguration).setInitialTerminalSize(size);
-//         this.terminal = factory.createTerminal();
-//         if(terminal instanceof SwingTerminalFrame swingTerminal) {
-//             swingTerminal.setResizable(false);
-//             swingTerminal.setTitle("🐱‍👤 Textadventure");
-//         }
-//         this.screen = new TerminalScreen(this.terminal);
-//         this.screen.startScreen();
-//         this.textGraphics = screen.newTextGraphics();
-//     }
-
-//     public void printMap(String map, int playerX, int playerY) throws IOException {
-//         String[] rows = map.split("\n");
-//         for(int i = 0; i < this.xSize + 2; i++) {
-//             putGreenBorder(i, 0);
-//         }
-//         for(int y = 0; y < rows.length; y++) {
-//             putGreenBorder(0, y + 1);
-//             String[] cells = rows[y].split("");
-//             for(int x = 0; x < cells.length; x++) {
-//                 putMazeTiles(x, y, cells, playerX, playerY);
-//             }
-//             putGreenBorder(xSize + 1, y + 1);
-//         }
-//         for(int i = 0; i < this.xSize + 2; i++) {
-//             putGreenBorder(i, ySize + 1);
-//         }
-//         putMapTitle();
-//         screen.refresh();
-//     }
-
-//     public void putMapTitle() {
-//         this.textGraphics.setForegroundColor(TextColor.ANSI.WHITE_BRIGHT);
-//         this.textGraphics.putString((this.xSize - 3) / 2, this.ySize + 1, "Karte", SGR.BOLD);
-//     }
-
-//     public void putGreenBorder(int x, int y) {
-//         this.textGraphics.setForegroundColor(TextColor.ANSI.YELLOW);
-//         this.textGraphics.setBackgroundColor(TextColor.ANSI.YELLOW);
-//         this.textGraphics.putString(x, y," ");
-//     }
-
-//     public void putMazeTiles(int x, int y, String[] cells, int playerX, int playerY) {
-//         this.textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
-//         this.textGraphics.setForegroundColor(TextColor.ANSI.BLUE_BRIGHT);
-//         if(mapFound || (x == playerX && y == playerY)) {
-//             this.textGraphics.putString(x + 1, y + 1, cells[x]);
-//         }
-//     }
-
-//     public void printRoomDescription(String description ,int height) throws IOException {
-//         this.textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
-//         this.textGraphics.setForegroundColor(TextColor.ANSI.CYAN_BRIGHT);
-//         for(int y = 0; y < 8; y++) {
-//             this.textGraphics.putString(40, height + y, "                                                               ");
-//         }
-//         screen.refresh();
-//         String[] rows = description.split("\n");
-//         for(int y = 0; y < rows.length; y++) {
-//             this.textGraphics.putString(40, height + y, rows[y]);
-//         }
-//         screen.refresh();
-//     }
-
-//     public void printDescription(String description ,int height) throws IOException {
-//         this.textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
-//         this.textGraphics.setForegroundColor(TextColor.ANSI.CYAN_BRIGHT);
-//         this.textGraphics.fillRectangle(new TerminalPosition(40, height), new TerminalSize(80, 8),' ');
-//         screen.refresh();
-//         String[] rows = description.split("\n");
-//         for(int y = 0; y < rows.length; y++) {
-//             this.textGraphics.putString(40, height + y, rows[y]);
-//         }
-//         screen.refresh();
-//     }
-
-//     public void printInputLine(ArrayList<KeyStroke> keyStrokes) throws IOException {
-//         this.textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
-//         this.textGraphics.setForegroundColor(TextColor.ANSI.WHITE_BRIGHT);
-//             for (int i = 0; i < 40; i++) {
-//                 this.textGraphics.putString(10 + i, 35, " ");
-//             }
-//         int j = 0;
-//         for(KeyStroke keyStroke : keyStrokes) {
-//             this.textGraphics.putString(10 + j, 35, keyStroke.getCharacter().toString());
-//             j++;
-//         }
-//         screen.refresh();
-//     }
-//     public void renderFrame() throws IOException {
-//         screen.refresh();
-//     }
-// }
