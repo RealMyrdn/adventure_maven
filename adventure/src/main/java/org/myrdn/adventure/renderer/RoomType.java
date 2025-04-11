a10389b3098e1fc3ce0c43f6b4a651dcc44758a1
realMyrdn <soeren.biester@gmail.com>
2025-04-11 06:07:36 +0200

    changed enum to class

--- adventure/src/main/java/org/myrdn/adventure/renderer/RoomType.java
+++ adventure/src/main/java/org/myrdn/adventure/renderer/RoomType.java
@@ -1,37 +1,10 @@
 package org.myrdn.adventure.renderer;
 
-public enum RoomType {
+public final class RoomType {
 
-    EMPTY(0, '█'),
-    DOOR_DOWN(1, '╻'),
-    DOOR_LEFT(2, '╸'),
-    CORNER_DL(3, '┓'),
-    DOOR_UP(4, '╹'),
-    DOORS_VERTICAL(5, '┃'),
-    CORNER_LU(6, '┛'),
-    T_LEFT(7, '┫'),
-    DOOR_RIGHT(8, '╺'),
-    CORNER_RD(9, '┏'),
-    DOORS_HORIZONTAL(10, '━'),
-    T_DOWN(11, '┳'),
-    CORNER_RU(12, '┗'),
-    T_RIGHT(13, '┣'),
-    T_UP(14, '┻'),
-    CROSS(15, '╋');
+    private final char[] symbol = {'█','╻','╸','┓','╹','┃','┛','┫','╺','┏','━','┳','┗','┣','┻','╋'};
 
-    private final int type;
-    private final char symbol;
-
-    RoomType(int type, char symbol) {
-        this.type = type;
-        this.symbol = symbol;
-    }
-
-    public char getSymbol() {
-        return symbol;
-    }
-
-    public int getType() {
-        return type;
+    public char getSymbol(int index) {
+        return symbol[index];
     }
 }
