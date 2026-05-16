76e445ecbeb528404ef4c85263dcdce87210a7f9
Sören <140298622+RealMyrdn@users.noreply.github.com>
2026-05-16 13:50:16 +0200

    Replace Layout maze generator with randomized DFS algorithm
    The previous probabilistic approach failed to guarantee full
    connectivity, causing random RuntimeExceptions on game start.
    The new DFS-based algorithm carves a spanning tree first, then
    adds ~20% extra connections for non-linear dungeon layouts.

--- src/main/java/org/myrdn/adventure/datahandler/Layout.java
+++ src/main/java/org/myrdn/adventure/datahandler/Layout.java
@@ -6,177 +6,104 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Layout {
-    
-    private static final Random RANDOM         = new Random();
-    private static final int DOOR_DOWN         = 0b0001;
-    private static final int DOOR_LEFT         = 0b0010;
-    private static final int DOOR_UP           = 0b0100;
-    private static final int DOOR_RIGHT        = 0b1000;
-    private static final int MAX_ROOM_ATTEMPTS = 10;
-    private static final int DOOR_THRESHOLD    = 1;
-    
+
+    private static final Random RANDOM     = new Random();
+    private static final int DOOR_DOWN     = 0b0001;
+    private static final int DOOR_LEFT     = 0b0010;
+    private static final int DOOR_UP       = 0b0100;
+    private static final int DOOR_RIGHT    = 0b1000;
+
+    private static final int[] DIR_DY  = { 1, -1,  0,  0};
+    private static final int[] DIR_DX  = { 0,  0, -1,  1};
+    private static final int[] DIR_CUR = {DOOR_DOWN, DOOR_UP,   DOOR_LEFT,  DOOR_RIGHT};
+    private static final int[] DIR_NBR = {DOOR_UP,   DOOR_DOWN, DOOR_RIGHT, DOOR_LEFT};
+
     static public int startPosY;
     static public int startPosX;
 
     private final int[][] layout;
     private final int xSize;
     private final int ySize;
-    
-    private int startTile;
-    
+
     public Layout(int xSize, int ySize) {
-
-        this.xSize = xSize;
-        this.ySize = ySize;
+        this.xSize  = xSize;
+        this.ySize  = ySize;
         this.layout = new int[ySize][xSize];
-
     }
 
-    public int getXSize() {
+    public int getXSize() { return this.xSize; }
+    public int getYSize() { return this.ySize; }
+    public int[][] getLayout() { return layout; }
 
-        return this.xSize;
-
+    public void buildLayout() {
+        initializeLayout();
+        generateStart();
+        dfsCarve(startPosY, startPosX, new boolean[ySize][xSize]);
+        addExtraConnections();
     }
 
-    public int getYSize() {
-
-        return this.ySize;
-
-    }
-
-    public int[][] getLayout() {
-
-        return layout;
-
-    }
-
-    private void initializeLayout(int xSize, int ySize) {
-    
-        for(int i = 0; i < ySize; i++) {
-    
-            for(int j = 0; j < xSize; j++) {
-    
-                this.layout[i][j] = 0;
-    
-            }
-    
+    private void initializeLayout() {
+        for (int y = 0; y < ySize; y++) {
+            Arrays.fill(layout[y], 0);
         }
-    
     }
 
     private void generateStart() {
-    
-        int minX                = this.xSize / 4;
-        int maxX                = minX + this.xSize / 2;
-        int xPos                = minX + RANDOM.nextInt(maxX - minX);
-        int yPos                = this.ySize - 1;
-        Layout.startPosX        = xPos;
-        Layout.startPosY        = yPos;
-        this.layout[yPos][xPos] = this.startTile;
-    
+        int minX        = xSize / 4;
+        int maxX        = minX + xSize / 2;
+        Layout.startPosX = minX + RANDOM.nextInt(maxX - minX);
+        Layout.startPosY = ySize - 1;
     }
 
-    private int generateStartTile() {
-    
-        ArrayList<Integer> tiles = new ArrayList<>(Arrays.asList(3,5,7,9,11,13,15));
-        Collections.shuffle(tiles);
-    
-        return tiles.get(0);
-    
-    }
+    private void dfsCarve(int y, int x, boolean[][] visited) {
+        visited[y][x] = true;
 
-    private void generateLayout() {
-    
-        for(int y = this.ySize-1; y >= 0; y--) {
-    
-            for(int x = 0; x < this.xSize; x++) {
-    
-                if(this.layout[y][x] == 0) {
-    
-                    int value = 0b0000;
-                    int counter = 0;
-    
-                    do {
-    
-                        counter++;
-                        value = checkDoors(x, y, value);
-                        this.layout[y][x] = value;
-    
-                        if(counter > MAX_ROOM_ATTEMPTS) {
-    
-                            break;
-    
-                        }
-    
-                    } while(value == 0);
-    
-                }
-    
+        ArrayList<Integer> order = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+        Collections.shuffle(order, RANDOM);
+
+        for (int i : order) {
+            int ny = y + DIR_DY[i];
+            int nx = x + DIR_DX[i];
+            if (ny >= 0 && ny < ySize && nx >= 0 && nx < xSize && !visited[ny][nx]) {
+                layout[y][x]   |= DIR_CUR[i];
+                layout[ny][nx] |= DIR_NBR[i];
+                dfsCarve(ny, nx, visited);
             }
-    
         }
-    
     }
 
-    private int checkDirection(int value, int door, int nextY, int nextX, int oppositeDoor) {
-    
-        if(nextY >= 0 && nextY < this.ySize && nextX >= 0 && nextX < this.xSize && (this.layout[nextY][nextX] == 0 || (oppositeDoor & this.layout[nextY][nextX]) != 0)) {
-    
-            value |= (this.layout[nextY][nextX] == 0 && RANDOM.nextInt(2) >= DOOR_THRESHOLD) ? 0 : door;
-    
+    private void addExtraConnections() {
+        for (int y = 0; y < ySize; y++) {
+            for (int x = 0; x < xSize; x++) {
+                if (RANDOM.nextInt(5) == 0) {
+                    int i  = RANDOM.nextInt(4);
+                    int ny = y + DIR_DY[i];
+                    int nx = x + DIR_DX[i];
+                    if (ny >= 0 && ny < ySize && nx >= 0 && nx < xSize) {
+                        layout[y][x]   |= DIR_CUR[i];
+                        layout[ny][nx] |= DIR_NBR[i];
+                    }
+                }
+            }
         }
-    
-        return value;
-    
-    }
-
-    private int checkDoors(int x, int y, int value) {
-    
-        value = checkDirection(value, DOOR_DOWN,  y + 1, x, 0b0100);
-        value = checkDirection(value, DOOR_LEFT,  y, x - 1, 0b1000);
-        value = checkDirection(value, DOOR_UP,    y - 1, x, 0b0001);
-        value = checkDirection(value, DOOR_RIGHT, y, x + 1, 0b0010);
-    
-        return value;
-    
     }
 
     public boolean isFullyConnected() {
-    
-        boolean[][] visited = new boolean[ySize][xSize];
-        int totalRooms = ySize * xSize;
-        int visitedRooms = depthFirstSearch(startPosY, startPosX, visited);
-    
-        return visitedRooms == totalRooms;
-    
+        boolean[][] visited  = new boolean[ySize][xSize];
+        int visitedRooms     = depthFirstSearch(startPosY, startPosX, visited);
+        return visitedRooms == ySize * xSize;
     }
 
     private int depthFirstSearch(int y, int x, boolean[][] visited) {
-    
-        if(y < 0 || y >= ySize || x < 0 || x >= xSize || layout[y][x] == 0 || visited[y][x]) {
-    
+        if (y < 0 || y >= ySize || x < 0 || x >= xSize || layout[y][x] == 0 || visited[y][x]) {
             return 0;
-    
         }
-        
         visited[y][x] = true;
         int count = 1;
-        
-        if((layout[y][x] & DOOR_DOWN)  != 0) count += depthFirstSearch(y + 1, x, visited);
-        if((layout[y][x] & DOOR_LEFT)  != 0) count += depthFirstSearch(y, x - 1, visited);
-        if((layout[y][x] & DOOR_UP)    != 0) count += depthFirstSearch(y - 1, x, visited);
-        if((layout[y][x] & DOOR_RIGHT) != 0) count += depthFirstSearch(y, x + 1, visited);
-        
+        if ((layout[y][x] & DOOR_DOWN)  != 0) count += depthFirstSearch(y + 1, x, visited);
+        if ((layout[y][x] & DOOR_LEFT)  != 0) count += depthFirstSearch(y, x - 1, visited);
+        if ((layout[y][x] & DOOR_UP)    != 0) count += depthFirstSearch(y - 1, x, visited);
+        if ((layout[y][x] & DOOR_RIGHT) != 0) count += depthFirstSearch(y, x + 1, visited);
         return count;
     }
-
-    public void buildLayout() {
-        
-        this.startTile = generateStartTile();
-        initializeLayout(this.xSize, this.ySize);
-        generateStart();
-        generateLayout();
-    
-    }
-
 }
